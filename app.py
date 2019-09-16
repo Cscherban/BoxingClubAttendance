fc73b1eb779468c354bcf0a65a703b1275f385c3
Cscherban <christopher.scherban@gmail.com>
2019-09-15 23:12:04 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, request,render_template, redirect
+import glob
+from recordProcessor import BoxingClubProcessor
+import os
+import sys
+from datetime import datetime
+from pytz import timezone
+import webbrowser
+from threading import Timer
+
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+if getattr(sys, 'frozen', False):
+    template_folder = resource_path('templates')
+    static_folder = resource_path('static')
+    current_dir = os.path.dirname(sys.argv[0]) + "/"
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else:
+    current_dir = ""
+    app = Flask(__name__)
+
+processor = None
+
+
+@app.route('/')
+def hello_world():
+    return render_template("start-screen.html")
+
+
+@app.route('/initialize', methods=['POST'])
+def init():
+    global processor
+    global current_dir
+    csvs = glob.glob(current_dir + '*.csv')
+    if len(csvs) != 0:
+        processor = BoxingClubProcessor(fileName=csvs[0])
+    return redirect('/table')
+
+
+@app.route('/set-date-today', methods=['POST'])
+def set_today_as_recording_date():
+    global processor
+    tz = timezone('US/Eastern')
+    date_string = datetime.now(tz).today().strftime('%m-%d-%Y')
+    processor.set_date(date_string)
+    processor.add_date_to_csv(date_string)
+    return redirect('/table')
+
+
+@app.route('/table', methods=['GET', 'POST'])
+def handle_table():
+    global processor
+    if processor is None:
+        return redirect('/')
+
+    date_set = processor.date is not None
+
+    if request.method == 'POST':
+        scanner_input = request.form.get('scannerInput')
+        gtid = processor.process_scanner_output(scanner_input)
+        print(gtid)
+        if gtid is None:
+            return render_template("table-visual.html", invalid="true", date_initialized=date_set,table_visual=processor.get_html())
+        print("his")
+        exists = processor.check_exists(gtid)
+        print exists
+        if exists:
+            processor.mark_as_attended(gtid)
+        return render_template("table-visual.html", exists=exists, date_initialized=date_set, table_visual=processor.get_html())
+    else:
+        return render_template("table-visual.html", table_visual=processor.get_html(), date_initialized=date_set)
+
+
+@app.route('/person/<gtid>', methods=['POST', 'DELETE'])
+def person_stuff(gtid):
+    if request.method == 'POST':
+        name = request.form.get('name')
+        processor.add_new_person(gtid, name)
+        return redirect("/table")
+    elif request.method == 'DELETE':
+        processor.remove_gtid(gtid)
+        return redirect("/table")
+
+
+@app.route('/date/', methods=['POST'])
+def remove_date():
+    date = request.form.get('date')
+    processor.remove_date(date)
+
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:2000/')
+
+
+if __name__ == '__main__':
+    Timer(1, open_browser).start()
+    app.run(port=2000)
---
+++ recordProcessor.py
@@ -0,0 +1,91 @@
+import numpy
+import pandas as pd
+import os.path
+
+
+class BoxingClubProcessor():
+
+    def __init__(self, fileName, date=None):
+        if not os.path.exists(fileName):
+            raise ValueError("Please provide a valid file")
+        self.csvFileName = fileName
+        self.csvAsDataFrame = pd.read_csv(fileName)
+        self.date = date
+
+    def set_date(self, date):
+        self.date = date
+
+    @staticmethod
+    def _extract_gtid(scanner_output):
+        split_output = scanner_output.split("=")
+        if len(split_output) < 3:
+            return None
+        res = scanner_output.split("=")[1]
+        if len(res) != 9:
+            return None
+
+        try:
+            i = int(res)
+            return i
+        except ValueError:
+            # Handle the exception
+            return None
+
+    def process_scanner_output(self, scanner_output):
+        return self._extract_gtid(scanner_output)
+
+    def get_dict(self):
+        return self.csvAsDataFrame.to_dict()
+
+    def get_html(self):
+        return self.csvAsDataFrame.to_html()
+
+    def add_date_to_csv(self, date):
+        df = self.csvAsDataFrame
+        print(df)
+        num_rows, num_cols = df.shape
+
+        try:
+            df.insert(loc=num_cols, column=date, value=["--" for i in range(num_rows)])
+        except:
+            return
+
+        df.to_csv(self.csvFileName,index=False)
+        self.csvAsDataFrame = df
+
+    def remove_date_from_csv(self, date):
+        df = self.csvAsDataFrame
+        df = df.drop(columns=date)
+        df.to_csv(self.csvFileName,index=False)
+        self.csvAsDataFrame = df
+
+    def add_new_person(self, gtid, name):
+        df = self.csvAsDataFrame
+        dict = {df.columns[i] : "--" for i in range(len(df.columns))}
+        dict["GTID"] = gtid
+        df = df.append(dict, ignore_index=True)
+        df.to_csv(self.csvFileName,index=False)
+        self.csvAsDataFrame = df
+
+    def mark_as_attended(self, gtid):
+        if self.date is None:
+            return False
+        df = self.csvAsDataFrame
+        df.loc[df["GTID"] == gtid, [self.date]] = "Present"
+        df.to_csv(self.csvFileName,index=False)
+
+    def remove_gtid(self, gtid):
+        df = self.csvAsDataFrame
+        df = df.drop(gtid, axis=0)
+        df.to_csv(self.csvFileName,index=False)
+        self.csvAsDataFrame = df
+
+    def check_exists(self, gtid):
+        df = self.csvAsDataFrame
+        if gtid == 0:
+            return False
+
+        for elem in df["GTID"]:
+            if gtid == elem:
+                return True
+        return False
